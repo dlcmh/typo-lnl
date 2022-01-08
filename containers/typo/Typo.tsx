7c4aafedc35b4445d9e440fc5565d7a7258867eb
David Chin <dlcmhd@me.com>
2022-01-08 12:35:57 +0800

    layout always show Scores

--- containers/typo/Typo.tsx
+++ containers/typo/Typo.tsx
@@ -14,32 +14,32 @@ export function Typo() {
   return (
     <AntdBasePage title="The Typo Challenge">
       <section className={styles.typo}>
-        <p>
-          <a href="https://github.com/dlcmh/typo-lnl">GitHub repo</a>
-        </p>
-        <h4>typoMachine {state.value}</h4>
-        {state.matches(State.loggedOut) && (
-          <Login
-            onLogin={({ userHandle }) => {
-              send(Event.LOG_IN, { userHandle })
-            }}
-          />
-        )}
+        <Row gutter={16}>
+          <Col span={12}>
+            <p>
+              <a href="https://github.com/dlcmh/typo-lnl">GitHub repo</a>
+            </p>
+            <h4>typoMachine {state.value}</h4>
+            {state.matches(State.loggedOut) && (
+              <Login
+                onLogin={({ userHandle }) => {
+                  send(Event.LOG_IN, { userHandle })
+                }}
+              />
+            )}
 
-        {state.matches(State.loggedIn) && (
-          <Profile onLogout={() => send(Event.LOG_OUT)} />
-        )}
+            {state.matches(State.loggedIn) && (
+              <Profile onLogout={() => send(Event.LOG_OUT)} />
+            )}
 
-        {state.matches(State.loggedIn) && (
-          <Row gutter={16}>
-            <Col span={12}>
+            {state.matches(State.loggedIn) && (
               <Challenge userHandle={state.context.userHandle} />
-            </Col>
-            <Col span={12}>
-              <Scores />
-            </Col>
-          </Row>
-        )}
+            )}
+          </Col>
+          <Col span={12}>
+            <Scores />
+          </Col>
+        </Row>
       </section>
     </AntdBasePage>
   )
